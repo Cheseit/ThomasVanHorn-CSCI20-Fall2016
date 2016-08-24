b4f3e83e3f77235306904e92bfebb9c36c9187cf
Cheseit <tvanhorn002@student.butte.edu>
2016-08-24 18:26:22 +0000

    ascci art

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,19 @@
+//Created by Thomas Van Horn
+//Created on August 24 2016
+
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    cout<<"^^^^^^^^^^^^^^^^^"<<endl;
+    cout<<"<                >"<<endl;
+    cout<<"<                >"<<endl;
+    cout<<"<                >"<<endl;
+    cout<<"<                >"<<endl;
+    cout<<"<                >"<<endl;
+    cout<<"<                >"<<endl;
+    cout<<"vvvvvvvvvvvvvvvvvv"<<endl;
+    
+}
